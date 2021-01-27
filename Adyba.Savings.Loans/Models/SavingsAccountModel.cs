db29a7796be803386aa8087634f5c6a41e6b1a81
Chiamaka-Uzuegbu <maureenclassy60@gmail.com>
2021-01-27 15:27:36 +0100

    Add project files.

---
+++ Adyba.Savings.Loans/Models/SavingsAccountModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Adyba.Savings.Loans.Models
+{
+    public class SavingsAccountModel
+    {
+        public string AccountNumber { get; set; }
+        public DateTime AccountCreationDate { get; set; }
+        public bool IsActive { get; set; }
+        public int AccountOwnerID { get; set; }
+    }
+}
